cc3b8dc8ebfd868548b6e89f780786fee88ee116
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-16 21:16:16 +0000

    h5m at the end of geometry script

--- cad_model.py
+++ cad_model.py
@@ -101,3 +101,15 @@ cq.exporters.export(inner_tank_wall, 'inner_tank_wall.stl')
 cq.exporters.export(outer_tank_wall, 'outer_tank_wall.stl')
 cq.exporters.export(lead, 'lead.stl')
 cq.exporters.export(flibe, 'flibe.stl')
+
+from stl_to_h5m import stl_to_h5m
+
+
+stl_filenames = ['inner_tank_wall.stl', 'outer_tank_wall.stl', 'lead.stl', 'flibe.stl']
+compentent_names = ['inner_tank_wall', 'outer_tank_wall', 'lead', 'flibe']
+
+stl_to_h5m(
+    files_with_tags=[(stl_filename, name) for name, stl_filename in zip(compentent_names, stl_filenames)],
+    h5m_filename='dagmc_not_merged.h5m',
+)
+

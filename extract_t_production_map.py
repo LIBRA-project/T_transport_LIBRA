5d0912db8ccd60c1edeba9a417e2ec6683ec8745
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-16 15:11:15 +0000

    added GPR

--- extract_t_production_map.py
+++ extract_t_production_map.py
@@ -66,7 +66,9 @@ statepoint = openmc.StatePoint(filepath=statepoint_file)
 t_prod_cyl = statepoint.get_tally(name="(n,Xt)_cylindrical")
 data = t_prod_cyl.get_pandas_dataframe()
 mean = np.array(data["mean"])*source_strength
+std_dev = np.array(data["std. dev."])*source_strength
 mean = reshape_values_to_mesh_shape(t_prod_cyl, mean)
+std_dev = reshape_values_to_mesh_shape(t_prod_cyl, std_dev)
 
 # # Interpolate data
 
@@ -96,3 +98,41 @@ plt.contourf(x_new, y_new, z, levels=np.linspace(0, mean.max(), 100), cmap='Purp
 plt.colorbar(image_map, ax=axs.ravel().tolist())
 plt.gca().set_aspect('equal')
 plt.savefig('real_vs_interpolated.png')
+
+
+# using GPR
+from inference.gp import GpRegressor, SquaredExponential, WhiteNoise
+
+Nx = 2
+Ny = 2
+xs = centers_x[::Nx]
+ys = centers_y[::Ny]
+data_points = [(i, j) for i in xs for j in ys]
+gpr = GpRegressor(
+    data_points, np.array(mean).T[::Nx, ::Ny].flatten(),
+    y_err=np.array(std_dev).T[::Nx, ::Ny].flatten(),
+    kernel=SquaredExponential
+)
+
+# x_new = mesh.r_grid[1::N]
+# y_new = mesh.z_grid[1::N]
+N_new = 100
+x_new = np.linspace(mesh.r_grid.min(), mesh.r_grid.max(), num=N_new)
+y_new = np.linspace(mesh.z_grid.min(), mesh.z_grid.max(), num=N_new)
+fit_points = [(i, j) for i in x_new for j in y_new]
+mu, sig = gpr(fit_points)
+
+fig, axs = plt.subplots(1, 2)
+plt.sca(axs[0])
+plt.gca().set_title('Mean (GPR)')
+plt.contourf(x_new, y_new, mu.reshape([N_new, N_new]).T, levels=100, cmap='Purples', vmin=0)
+plt.gca().set_aspect('equal')
+plt.colorbar(label="mean")
+
+plt.sca(axs[1])
+plt.gca().set_title('Std Dev (GPR)')
+plt.contourf(x_new, y_new, sig.reshape([N_new, N_new]).T, levels=100, cmap='Purples', vmin=0)
+plt.gca().set_aspect('equal')
+plt.colorbar(label="std dev")
+
+plt.savefig('from_gpr.png')
